d36a80bfe6737bf6745501aa34afbbca3acf2faa
EC2 Default User <ec2-user@ip-172-31-45-240.ec2.internal>
2022-01-15 08:43:10 +0900

    [Update]220115 08:43

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,24 +1,32 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
 
   def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(current_user)
-
-     #flash[:create] ="You have created book successfully!"
+    book = Book.new(book_params)
+    book.save
+    redirect_to  book_path
+  #@book = Book.new(book_params)
+  # @book.user_id = current_user.id
+  #@book.save
+  #redirect_to
+      #flash[:create] ="You have created book successfully!"
   end
 
   def index
      @book = Book.new
      @books = Book.all
-     @user = User.find(session[:user_id])
+     @user = current_user
   end
 
   def show
-    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)  
   end
 
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,26 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(session[:user_id])
+    @user = User.find(params[:id])
     @books = @user.books
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+   @user = User.find(params[:id])
+   @user.update(user_params)
+   redirect_to user_path(@user.id)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
+  def book_params
+    params.permit(:title, :body, :user_id)
   end
 end
