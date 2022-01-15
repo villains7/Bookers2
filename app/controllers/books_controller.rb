d8224d20ee3f931bb09c330738a328c311247051
EC2 Default User <ec2-user@ip-172-31-45-240.ec2.internal>
2022-01-15 20:50:13 +0900

    [Update] intrのリンク先確認　220115 20:50

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,7 +14,7 @@ class BooksController < ApplicationController
   def index
      @book = Book.new
      @books = Book.all
-     @user = current_user
+     @user = User.find(current_user.id)
   end
 
   def show
@@ -31,6 +31,9 @@ class BooksController < ApplicationController
   end
 
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
     flash[:sestroy] ="You have deleted book successfully!"
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,11 @@ class UsersController < ApplicationController
    flash[:user_update] ="You have updated user successfully!"
   end
 
+  def index
+    @users = User.all
+    @user = User.find(current_user.id)
+
+  end
   private
 
   def user_params
