105af2f80965f49aad8c0e5025e9883af6f4e244
EC2 Default User <ec2-user@ip-172-31-45-240.ec2.internal>
2022-01-16 10:00:49 +0900

    [Update] All link ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,18 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters,if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-   current_user
+   user_path(current_user)
   end
 
   def after_sign_up_path_for(resource)
-    current_user
+   user_path(resource)
   end
 
-  def after_sign_out_path_for(resource_or_scope)
+
+  def after_sign_out_path_for(resource)
     root_path
   end
+
 
   protected
 
