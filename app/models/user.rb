3a6a3b587a4170fbeb3b8ae6bb63335593298a49
EC2 Default User <ec2-user@ip-172-31-45-240.ec2.internal>
2022-02-07 19:26:35 +0900

    いいねコメント機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_one_attached :profile_image
   has_many :books,dependent: :destroy
+  has_many :book_comments, dependent: :destroy
 
   validates :name, uniqueness: true, length: {minimum: 2,maximum: 20}
   validates :introduction,length: {maximum: 50}
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'favorites/create'
+  get 'favorites/destroy'
 devise_for :users
 devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
@@ -11,7 +13,11 @@ root to: "homes#top"
   devise_scope :user do
   post 'users/guest_sign_in',to: 'users/sessions#guest_sign_in'
   end
-resources :books,only: [:index,:show,:create,:edit,:update,:destroy]
+resources :books,only: [:index,:show,:create,:edit,:update,:destroy] do
+  resource :favorites, only: [:create,:destroy]
+  resources :book_comments, only: [:create,:destroy]
+
+end
 resources :users, only: [:show,:edit,:update,:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
